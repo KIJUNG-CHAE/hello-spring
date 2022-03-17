c6178fc2964a448ae24a68a90e199591f9c4b29d
kijung <daiywlf5251@gmail.com>
2022-03-17 14:56:14 +0900

    add Repository test case

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -8,15 +8,15 @@ public class Member {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
